e22dd456c009d31c643782877a25421c6b42c4d5
hojjatAllah1371 <hojjat.rezapoor95@gmail.com>
2024-03-01 18:34:50 +0330

    some exercise

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const request = require("request");
+const chalk = require("chalk");
 
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?lat=37.2793607&lon=49.5846102&appid=3db603f4461ea7190804a28ea42deef4&units=metric";
+// const rashtReq =
+//   "https://api.openweathermap.org/data/2.5/weather?lat=37.2793607&lon=49.5846102&appid=3db603f4461ea7190804a28ea42deef4&units=metric";
 
-request({ url: url, json: true }, (error, response) => {
-  const data = response.body;
-  const cTemp = data.main.temp;
-  const feelsTemp = data.main.feels_like;
-  console.log(`Location: ${data.name}-${data.sys.country}`);
-  console.log(
-    `Curent temp is ${cTemp}°C feels like ${feelsTemp}°C`
-  );
+// request({ url: rashtReq, json: true }, (error, response) => {
+//   const data = response.body;
+//   const cTemp = data.main.temp;
+//   const feelsTemp = data.main.feels_like;
+//   console.log(`Location: ${data.name}-${data.sys.country}`);
+//   console.log(
+//     `Curent temp is ${cTemp}°C feels like ${feelsTemp}°C`
+//   );
+// });
+
+//challange req
+const challReq ="http://api.openweathermap.org/geo/1.0/direct?q=London&appid=3db603f4461ea7190804a28ea42deef4";
+
+request({ url: challReq, json: true }, (error, response) => {
+  if (error) { //internet connection error
+    console.log(chalk.blue.inverse("Check your internet connection!"));
+    //error handeling from API response
+  } else if (response.body.message) {
+      const errorMessage = response.body.message;
+      console.log(chalk.red.inverse(errorMessage));
+    } else {
+      const result = response.body[0];
+      console.log(`The City name is ${result.name}`);
+      console.log(`The Coordinate lat: ${result.lat} long: ${result.long}`);
+    }
 });
